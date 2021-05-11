9ac0f8d86a731d09b3cf0a1e00dea3fa77bdf1b3
RavindraMeduri <nagavenkat6070@gmail.com>
2021-05-10 23:10:37 -0500

    APIs updated using callbacks

--- routes/todo.route.js
+++ routes/todo.route.js
@@ -1,54 +1,95 @@
 const router = require('express').Router();
+const path = require('path');
+var { getFileContent, writeFileContent } = require('../utils/file.utils.js');
+var fileName = path.join(__dirname, '..', 'db', 'todo.db.json');
 
-var todos = [];
-
-function getNextId() {
-    return todos.length == 0 ? 1 : todos[todos.length - 1].id + 1;
+function getNextId(cb) {
+    getFileContent(fileName, function (err, todos) {
+        if(err) cb(err, null);
+        return cb(null, todos.length == 0 ? 1 : todos[todos.length - 1].id + 1, todos);
+    });
 }
 
 router.get('/', function (req, res) {
-    res.json(todos);
+   
+    getFileContent(fileName, function (err, todos) {
+        if (err) throw err;
+        res.json(todos);
+    });
 });
 
 router.post('/', function (req, res) {
-    var id = getNextId();
-    var todo = {...req.body, id : id};
-    todos.push(todo);
-    res.send(todo);
+    getNextId(function (err, id, todos) {
+        if (err) throw err;
+        var todo = {...req.body, id : id};
+        todos.push(todo);
+        writeFileContent(fileName, todos, function (err, data) {
+            if (err) throw err;
+            res.send(todo);
+        });
+    });
+   
 });
 
 router.put('/:id', function (req, res) {
+
     var { id } = req.params;
     var updatedTodo = req.body;
-    var idx = todos.findIndex(t => t.id == id);
-    if(idx != -1) {
-        todos[idx] = {...updatedTodo, id : id};
-        return res.send({...updatedTodo, id : id});
-    }
+    
+    getFileContent(fileName, function (err, todos) {
+        if (err) throw err;
+        var idx = todos.findIndex(t => t.id == id);
+        if(idx != -1) {
+            todos[idx] = {...updatedTodo, id : id};
+            return writeFileContent(fileName, todos, function (err) {
+                if (err) throw err;
+                return res.send({...updatedTodo, id : id});
+            });
+        
+        }
 
-    return json({
-        msg : 'Todo with the specified id does not exist'
+        return res.json({
+            msg : 'Todo with the specified id does not exist'
+        });
     });
+
 });
 
 router.patch('/:id', function (req, res) {
     var { id } = req.params;
     var updatedTodo = req.body;
-    var idx = todos.findIndex(t  => t.id == id);
-    if(idx != -1) {
-        todos[idx] = {...todos[idx], ...updatedTodo, id : id};
-        return res.send({...todos[idx], ...updatedTodo, id : id});
-    }
+    getFileContent(fileName, function (err, todos) {
+        if (err) throw err;
+        var idx = todos.findIndex(t  => t.id == id);
+        if(idx != -1) {
+            todos[idx] = {...todos[idx], ...updatedTodo, id : id};
+            return writeFileContent(fileName, todos, function (err) {
+                if (err) throw err;
+                return res.json({...todos[idx], ...updatedTodo, id : id});
+            });
+        
+        }
 
-    return json({
-        msg : 'Todo with the specified id does not exist'
+        return res.json({
+            msg : 'Todo with the specified id does not exist'
+        });
     });
+    
 });
 
 router.delete('/:id', function (req, res) {
+
     var { id } = req.params;
-    todos = todos.filter(t => t.id != id);
-    res.json(todos);
+
+    getFileContent(fileName, function (err, todos) {
+        if (err) throw err;
+        todos = todos.filter(t => t.id != id);
+        return writeFileContent(fileName, todos, function (err) {
+            if (err) throw err;
+            res.json({});
+        });
+    });
+    
 });
 
 module.exports = router;
---
+++ utils/file.utils.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+exports.getFileContent = function(fileName, cb) {
+    fs.readFile(fileName, function (err, data) {
+        if(err) return cb(err, null);
+        return cb(null, JSON.parse(data.toString()));
+    });
+}
+
+exports.writeFileContent = function(fileName, data, cb) {
+    fs.writeFile(fileName, JSON.stringify(data), function (err, data) {
+        if(err) return cb(err, null);
+        return cb(null);
+    });
+}
+
